c0b04bf025a0330754d4db6a48744d1fca6d5aab
Your Name <you@example.com>
2022-12-19 15:38:28 +0200

    fixed code

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,20 @@ public class Main {
         Random random = new Random();
         int[] numbersFirm = new int [7];
         for(int i = 0; i < numbersFirm.length; i++){
-            numbersFirm[i] = random.nextInt( 9 - 0) + 0;
-            Arrays.sort(numbersFirm);
-            System.out.print(numbersFirm[i] + " ");
+            numbersFirm[i] = random.nextInt( 10 - 1) + 1;
+
         }
+        Arrays.sort(numbersFirm);
+        System.out.print(Arrays.toString(numbersFirm) + " ");
         System.out.println();
         int[] guessednumbers = new int [7];
         for(int j = 0; j < guessednumbers.length; j++){
-            guessednumbers[j] = random.nextInt(9 -0) + 0;
-            Arrays.sort(guessednumbers);
-            System.out.print(guessednumbers[j] + " ");
+            guessednumbers[j] = random.nextInt(10 -1) + 1;
+
+
         }
+        Arrays.sort(guessednumbers);
+        System.out.print(Arrays.toString(guessednumbers) + " ");
         System.out.println();
         int count = 0;
         for ( int i = 0; i < numbersFirm.length && i < guessednumbers.length; i++){
